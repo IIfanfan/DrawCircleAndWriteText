32c9b251c0a747f7d23649431fec730b9c7643b8
TianLiangFan <1092924979@qq.com>
2021-04-25 10:40:10 +0800

    修改bug和添加命名空间，加上使用方法添加项目文件。

---
+++ DrawCircleAndWriteText/DrawAgvCarAndWriteText.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RP.AGVDispatch.Monitor
+{
+    /// <summary>
+    /// DrawAgvCarAndWriteText.xaml 的交互逻辑
+    /// </summary>
+    public partial class DrawAgvCarAndWriteText : UserControl
+    {
+        public DrawAgvCarAndWriteText()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 设置文本
+        /// </summary>
+        public string WriteText { get; set; }
+        /// <summary>
+        /// 设置颜色
+        /// </summary>
+        public Brush color { get; set; }
+
+        private void Rectangle_Loaded(object sender, RoutedEventArgs e)
+        {
+            TextName.Text = WriteText;
+            TextRectangle.Fill = color;
+        }
+    }
+}
---
+++ DrawCircleAndWriteText/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using RP.AGVDispatch.Monitor;
+
+namespace DrawCircleAndWriteText
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DrawAgvCarAndWriteText drawAgvCarAnd = new DrawAgvCarAndWriteText();    //使用其中一个自定义控件
+            canvas.Children.Add(drawAgvCarAnd);
+        }
+    }
+}
